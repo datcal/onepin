2a5b79e67be7e91c938b231d959ec24894723c7b
Burak Hamza <thedatcal@gmail.com>
2021-02-15 16:43:13 +0300

    add style and schema

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -5,14 +5,26 @@ export default class Main extends Component {
   
   render() {
     return (
-        <div class="container">
-            <div class="row">
-                <div class="col">Burak Hamza</div>
+      <div>
+          <div class="wrapper">
+            <div class="resim">
+                <img src="./images/loginbg.jpg" />
             </div>
-            <div class="row">
-                <div class="col-12">@twitter</div>
-            </div>
-        </div>
+            <h1 class="text-center">Burak Hamza <i class="fa fa-check-circle" aria-hidden="true"></i></h1>
+            <h4 class="text-center">@paul</h4>
+            <p class="text-center mt-4">Designer, Front End Developer, WordPress Developer. Judoka.</p>
+
+          </div>
+          <div class="buttons">
+              <div class="mb-2 mt-2 text-center">
+                  <a href="javascript:;" class="d-block rounded-0 mt-2 mb-2 pt-2 pb-2 w-100 btn btn-danger">My Portfolio</a>
+                  <a href="javascript:;" class="d-block rounded-0 mt-2 mb-2 pt-2 pb-2 w-100 btn btn-danger">My Portfolio</a>
+                  <a href="javascript:;" class="d-block rounded-0 mt-2 mb-2 pt-2 pb-2 w-100 btn btn-danger">My Portfolio</a>
+                  <a href="javascript:;" class="d-block rounded-0 mt-2 mb-2 pt-2 pb-2 w-100 btn btn-danger">My Portfolio</a>
+                  <a href="javascript:;" class="d-block rounded-0 mt-2 mb-2 pt-2 pb-2 w-100 btn btn-danger">My Portfolio</a>
+              </div>
+          </div>
+      </div>
     )
   }
 }
