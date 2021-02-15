73a0364a2cf04aa614c0f3499c8f67125718496e
Burak Hamza <burak.hamza@mivento.com>
2021-02-15 15:58:41 +0300

    add some files

---
+++ src/components/main/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+export default class Main extends Component {
+  
+  render() {
+    return (
+        <div class="container">
+            <div class="row">
+                <div class="col">Burak Hamza</div>
+            </div>
+            <div class="row">
+                <div class="col-12">@twitter</div>
+            </div>
+        </div>
+    )
+  }
+}
